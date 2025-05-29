83e0594469e6a7883cf2ccab751771ed82159fff
dgd03146 <ibory1220@naver.com>
2025-05-29 19:53:47 +0900

    feat: Redux 스토어 및 커스텀 훅 추가

---
+++ src/store/index.ts
@@ -0,0 +1,31 @@
+import { eventsSlice } from '@/features/calendar/store/eventsSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['events'],
+};
+
+const rootReducer = combineReducers({
+  events: eventsSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
