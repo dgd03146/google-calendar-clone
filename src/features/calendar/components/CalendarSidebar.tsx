abac48db4b6f08837e7d0ebeef4f034f3ac6b1ee
dgd03146 <ibory1220@naver.com>
2025-05-29 23:40:54 +0900

    feat: 캘린더 사이드바에서 이벤트 생성 모달 열기 기능 추가 및 상태 관리 개선

--- src/features/calendar/components/CalendarSidebar.tsx
+++ src/features/calendar/components/CalendarSidebar.tsx
@@ -1,23 +1,37 @@
+import { useAppDispatch } from '@/store/hooks';
 import { Plus } from 'lucide-react';
-import { useState } from 'react';
+import { openCreateModal } from '../store/eventsModalSlice';
 import { MiniCalendar } from './MiniCalendar';
 
 export const CalendarSidebar = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const dispatch = useAppDispatch();
 
-  const handleDateChange = (date: Date) => {
-    setCurrentDate(date);
+  const handleCreateClick = () => {
+    const now = new Date();
+    const currentHour = now.getHours();
+    const currentMinute = now.getMinutes();
+
+    dispatch(
+      openCreateModal({
+        date: now,
+        hour: currentHour,
+        minute: currentMinute,
+      })
+    );
   };
 
   return (
     <aside className="w-[256px] bg-background border-r border-gray-200 flex flex-col py-4 px-4 min-h-[calc(100dvh-64px)]">
       <div className="sticky top-[80px] z-10">
-        <button className=" max-w-fit py-4 px-8 mb-6 text-sm text-gray-500 flex items-center gap-2 bg-white border-b border-gray-200 hover:bg-gray-100 cursor-pointer rounded-2xl shadow-lg transition-all duration-300">
+        <button
+          onClick={handleCreateClick}
+          className="max-w-fit py-4 px-8 mb-6 text-sm text-gray-500 flex items-center gap-2 bg-white border-b border-gray-200 hover:bg-gray-100 cursor-pointer rounded-2xl shadow-lg transition-all duration-300"
+        >
           <Plus className="h-4 w-4" />
           <span className="text-md font-medium">만들기</span>
         </button>
 
-        <MiniCalendar selectedDate={currentDate} onDateSelect={handleDateChange} />
+        <MiniCalendar />
       </div>
     </aside>
   );
